d9270b1362661986ecda56352086032d2a7dcc5a
Georg Federmann <georg.federmann@gmail.com>
2021-01-12 16:19:46 +0100

    replace map with for

--- giggin/src/giggin/components/gigs.cljs
+++ giggin/src/giggin/components/gigs.cljs
@@ -6,12 +6,14 @@
   ;; gig items to the user.
   ;; Do not use for/while loop, but map function.
   [:main
-    [:div.gigs (map (fn [gig] 
-                      [:div.gig {:key (:id gig)}
-                       [:img.gig__artwork {:src (:img gig) :alt "alt"}]
-                       [:div.gig__body
-                        [:div.gig__title
-                         [:div.btn.btn--primary.float--right.tooltip {:data-tooltip "Add to order"}
-                          [:i.icon.icon--plus]] (:title gig) ]
-                        [:p.gig__price (:price gig)]
-                        [:p.gig__desc (:desc gig)]]]) (vals @state/gigs))]])
+    [:div.gigs
+
+     (for [gig (vals @state/gigs)]
+       [:div.gig {:key (:id gig)}
+        [:img.gig__artwork {:src (:img gig) :alt "alt"}]
+        [:div.gig__body
+         [:div.gig__title
+          [:div.btn.btn--primary.float--right.tooltip {:data-tooltip "Add to order"}
+           [:i.icon.icon--plus]] (:title gig)]
+         [:p.gig__price (:price gig)]
+         [:p.gig__desc (:desc gig)]]])]])
