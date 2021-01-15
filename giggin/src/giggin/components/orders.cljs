4a3a0a8acbcbffb4d591b3bfb8813e05caef359e
Georg Federmann <georg.federmann@gmail.com>
2021-01-15 19:22:13 +0100

    update view on event

--- giggin/src/giggin/components/orders.cljs
+++ giggin/src/giggin/components/orders.cljs
@@ -1,5 +1,16 @@
-(ns giggin.components.orders)
+(ns giggin.components.orders
+  (:require [giggin.state :as state]))
 
 (defn orders []
-  [:orders "orders"])
-orders
+  [:aside
+   [:div.order
+    [:div.gig__body
+     (for [[id quant] @state/orders] ;; e.g. {:gig-11 3, :gig-06 2, :gig-05 8, :gig-09 3}
+       [:div.item {:key id}
+        [:div.img
+         [:img {:src (get-in @state/gigs [id :img])
+                :alt (get-in @state/gigs [id :title])}]]
+        [:div.content
+         [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quant)]]
+        [:div.action
+         [:div.price (* (get-in @state/gigs [id :price]) quant)]]])]]])
