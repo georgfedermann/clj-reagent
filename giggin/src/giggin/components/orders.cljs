c09d8fa3bb9f3f0835d8bb8dc18b3b7a528864de
Georg Federmann <georg.federmann@gmail.com>
2021-01-16 12:29:47 +0100

    add event to remove order items

--- giggin/src/giggin/components/orders.cljs
+++ giggin/src/giggin/components/orders.cljs
@@ -13,4 +13,8 @@
         [:div.content
          [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quant)]]
         [:div.action
-         [:div.price (* (get-in @state/gigs [id :price]) quant)]]])]]])
+         [:div.price (* (get-in @state/gigs [id :price]) quant)]
+         ;; add event listener to remove items from the order list
+         [:button.btn.btn--link.tooltip {:data-tooltip "Remove"
+                                         :on-click (fn [] (swap! state/orders dissoc id))}
+          [:i.icon.icon--cross]]]])]]])
